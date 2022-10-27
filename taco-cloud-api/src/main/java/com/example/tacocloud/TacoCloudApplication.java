6161a964b96044e0417db9516642144b9e6f453d
Vyacheslav Basov <vyacheslav.basov@idftechnology.by>
2022-10-28 00:24:30 +0300

    resolve issues with bean scope

--- taco-cloud-api/src/main/java/com/example/tacocloud/TacoCloudApplication.java
+++ taco-cloud-api/src/main/java/com/example/tacocloud/TacoCloudApplication.java
@@ -2,10 +2,14 @@ package com.example.tacocloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.example.model"})
+@ComponentScan(basePackages = {"com.example.*"})
+@EntityScan(basePackages = {"com.example.*"})
+@EnableJpaRepositories(basePackages = {"com.example.*"})
 public class TacoCloudApplication {
 
   public static void main(String[] args) {
